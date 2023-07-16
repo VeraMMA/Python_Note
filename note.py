f01cdd0c023021f46a0b725ea716c5344c50fdbf
Vera Maria <mymadonna89@mail.ru>
2023-07-16 14:32:29 +0600

    Добавлены формат текста и меню

---
+++ note.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+root = Tk()
+root.title('Заметки')
+root.geometry('600x700')
+
+main_menu = Menu(root)
+
+file_menu = Menu(main_menu, tearoff=0)
+file_menu.add_command(label='Открыть')
+file_menu.add_command(label='Сохранить')
+file_menu.add_separator()
+file_menu.add_command(label='Закрыть')
+
+root.config(menu=file_menu)
+
+main_menu.add_cascade(label='Меню', menu=file_menu)
+
+root.config(menu=main_menu)
+
+f_text = Frame(root)
+f_text.pack(fill=BOTH, expand=1)
+
+text_fild = Text(f_text, bg='black', fg='ivory', padx=15, pady=15, 
+                 wrap=WORD, insertbackground='yellow', selectbackground='#9D928B', spacing3=10)
+text_fild.pack(expand=1, fill=BOTH, side=LEFT)
+
+root.mainloop()
