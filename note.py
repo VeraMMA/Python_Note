5896ec28b38501241009e0b32e89a09eb47ee1d8
Vera Maria <mymadonna89@mail.ru>
2023-07-16 15:22:41 +0600

    Добавлена функция открытия файлов

--- note.py
+++ note.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox 
+from tkinter import filedialog
 
 root = Tk()
 root.title('Заметки')
@@ -10,11 +11,17 @@ def note_exit():
     if answer:
         root.destroy()
 
+def open_files():
+    file_path = filedialog.askopenfilename(title='Выбрать файл', filetypes=(('Текстовые документы(*.txt)','*.txt'),('Все файлы', '*.*')))        
+    if file_path:
+        text_fild.delete('1.0', END)
+        text_fild.insert('1.0', open(file_path, encoding='utf-8').read())
+
 main_menu = Menu(root)
     
 
 file_menu = Menu(main_menu, tearoff=0)
-file_menu.add_command(label='Открыть')
+file_menu.add_command(label='Открыть', command=open_files)
 file_menu.add_command(label='Сохранить')
 file_menu.add_separator()
 file_menu.add_command(label='Закрыть', command=note_exit)
